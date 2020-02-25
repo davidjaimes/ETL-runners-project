c19a0af662c2b86eb7cc3184941d6f1c50eecd3e
David Jaimes <david@djaimes.com>
2020-02-24 19:54:33 -0800

    add SQL code

---
+++ jaimes/runner-results.sql
@@ -0,0 +1,13 @@
+create table results (
+	runnerid as int primary key,
+	firstname as varchar,
+	lastname as varchar,
+	age as int,
+	bib as int,
+	birthdate as varchar,
+	gender as varchar,
+	overallplace as int,
+	overalltime as varchar
+);
+
+select * from results;
